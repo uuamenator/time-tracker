e523e80da7e69e4682770e296a82eaaaefa619f0
uuamenator <uuamenator@gmail.com>
2017-12-03 14:14:49 +0200

    Initial commit

---
+++ src/main/java/com/gmail/uuamenator/timetracker/controllers/SiteController.java
@@ -0,0 +1,16 @@
+package com.gmail.uuamenator.timetracker.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class SiteController {
+
+    @GetMapping("")
+    @ResponseBody
+    public String home(){
+        return "Hasta La Vista";
+    }
+}
